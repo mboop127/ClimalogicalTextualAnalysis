afc6dfe38dcda87c6f6d81f8e26359962a89c250
Jackson Chen <jackson.chen@columbia.edu>
2018-09-28 18:41:09 -0400

    Include total number of words in results

--- wordFreq.py
+++ wordFreq.py
@@ -5,18 +5,17 @@ import os
 def parseBook(inputFile):
     bookFile = open(inputFile, 'r') # Establish a file reader
     book = bookFile.read() # Read the file
-    bookWithoutPunctuation = book.translate(None, string.punctuation) # Get rid of punctuation
-    bookList = re.split(' |\r\n', bookWithoutPunctuation) # Split by space or newline
+    bookWithoutPunctuation = book.translate(str.maketrans('','',string.punctuation)) # Get rid of punctuation
+    bookList = re.split(' |\r\n|\n', bookWithoutPunctuation) # Split by space or newline
     bookListWithoutEmpty = list(filter(None, bookList)) # Remove empty elements
     cleanBook = map(lambda word: word.lower(), bookListWithoutEmpty) # Convert all letters to lowercase
-    return cleanBook
+    return list(cleanBook)
 
 wordFile = open('Word-list.txt', 'r')
-masterList = map(lambda word: word.strip().lower(), wordFile.read().split(','))
+masterList = list(map(lambda word: word.strip().lower(), wordFile.read().split(',')))
 
 with open('results.csv', 'w') as f:
     for filename in os.listdir(os.getcwd() + '/books'):
-        f.write('\n' + filename + ',Count\n')
 
         dict = {} # Establish a dictionary
         wordList = parseBook('books/' + filename)
@@ -26,6 +25,7 @@ with open('results.csv', 'w') as f:
             else:
                 dict[word] = 1
 
+        f.write('\n' + filename + ',' + str(len(wordList)) + '\n')
         for word in masterList:
             if word in dict:
                 f.write(word + ',' + str(dict[word]) + '\n')
